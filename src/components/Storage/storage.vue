133454db7d527f83b2512101caac47ad92e822c2
rzkhosroshahi <rzkhkosroshahi@gmail.com>
2022-06-05 11:08:12 +0430

    change the way of rendering storage

--- src/components/Storage/storage.vue
+++ src/components/Storage/storage.vue
@@ -2,29 +2,12 @@
   <div>storage</div>
 </template>
 <script>
-import { MountStorage, UnMountStorage } from 'storage/StorageApp';
+import { MountStorage } from 'storage/StorageApp';
 
 export default {
   name: 'Storage',
-
   mounted() {
-    // MountStorage('content');
-  },
-  beforeMount() {
     MountStorage('content');
   },
-  beforeDestroy() {
-    UnMountStorage('content')
-  },
-  watch: {
-    $route(to, from) {
-      console.log('to >>', to);
-      this.$router.replace(to);
-      if (to !== from) {
-        console.log('hey >>');
-        this.$router.push(to);
-      }
-    }
-  }
 }
 </script>
