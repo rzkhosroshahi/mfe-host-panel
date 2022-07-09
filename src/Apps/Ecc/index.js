d7c94b26a269abec74f4e94021389bdd8fe7050c
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2022-07-09 21:43:48 +0430

    Update index.js

--- src/Apps/Ecc/index.js
+++ src/Apps/Ecc/index.js
@@ -3,6 +3,7 @@ import registerApp from "../../Modules/registerApp";
 const ECC_REMOTE = process.env.ECC_REMOTE;
 
 console.log('process.env.ECC_REMOTE >>', process.env.ECC_REMOTE);
+console.log('process.env.ASSET_PATH >>', process.env.ASSET_PATH);
 const config = {
     remoteUrl: ECC_REMOTE,
     scope: 'ecc',
